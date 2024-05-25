9b1a2ee03c34f00825378ac8ee2a75be44c7b765
Stefanija <smorozova0808@gmail.com>
2024-05-25 17:51:45 +0300

    Changed GetCandidate to smth else
    Previous version did not work, using new version.

--- src/main/java/app/GetCandidate.java
+++ src/main/java/app/GetCandidate.java
@@ -1,73 +1,135 @@
 package app;
 import java.io.IOException;
-import java.sql.*;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.*;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;/**
- * 
- */
-public class GetCandidate {
- 
-	/**
-	 * @param args
-	 */
-	/**
-	 * 
-	 * 
-	 * @param args
-	 * @throws SQLException
-	 */
-	public static void main(String[] args) throws SQLException{
-//		makes a connection to the SQL database, and exception for when the connection/login using variables below fails
-		
-		String driver = "com.mysql.jdbc.Driver";
-		String DBpath = "//localhost/"; ///add local host after dealing with database
-		String username = " ";
-		String password = " ";
-		Connection con = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-	//exception and sql instance
-	    try {
-		Class.forName(driver).newInstance();} 
-	    catch (Exception ex) {
-		Logger.getLogger(GetCandidate.class.getName()).log(Level.SEVERE, "Whoopsie, connection turbulent or disconnected", ex);}
-	//solidifies connection
-	    con = DriverManager.getConnection("jdbc:mysql:" + DBpath, username, password);
-	    stmt = con.createStatement();
-	    
-	    //makes a sql query via string that can be executed and printed as a statement
-	    String sql;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+//set up the webservlet for getting a list of all the candidates
+@WebServlet(
+    name = "getCandidates",
+    urlPatterns = {"/getcandidates"})
+//public class connected to servlets
+public class GetCandidate extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+	  
+	  String driver = "com.mysql.jdbc.Driver";
+		String DBpath = "//localhost/ "; //add database path
+		String username = "root"; //user name
+		String password = " "; //add password
+		Connection connection = null; //responsible for connection
+		Statement statement = null; //responsible for query statement
+		ResultSet result = null; //responsible for result of query statement
+
+		try {
+			//setting up class instance
+			Class.forName(driver).newInstance();
+		} catch (Exception ex) {
+			Logger.getLogger(GetCandidate.class.getName()).log(Level.SEVERE, "Whopsie, can't do that!", ex);
+		}
+		//setting up mysql connection 
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql:" + DBpath, username, password);
+		} catch (SQLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		try {
+			statement = connection.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		//sets up sql statement as a string
+
+		String sql;
+        
         sql = "Select*From candidates";
-        rs = stmt.executeQuery(sql);
-        System.out.print(rs);
-        
-        
-        //array list for candidate bastards
-        
-        ArrayList<String> CANDIDATE = new ArrayList<String>();
-
         try {
-            int i = rs.getInt("ID"); // There is also other version for getInt which relies on column index number
-            System.out.println("i: " + i + "\n");
+        	//converts string into query and executes it
+			result = statement.executeQuery(sql);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        System.out.print(result);
+        
+        try {
+            int i = result.getInt("ID"); // There is also other version for getInt which relies on column index number
+            System.out.println("i: " + i + "\n");    
+            
         } catch (Exception ex) {
-            System.out.println("*** Does not work without first using rs.next()!!! ***");
-            // Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-         // Going through the results
-            while (rs.next()) {
-            	int a = rs.getInt("CANDIDATE_ID");
-                String i = rs.getString("FIRST_NAME");
-                String e = rs.getString("LAST_NAME");
-                CANDIDATE.add(i);
-                System.out.println(a);
-                System.out.println(i);
-                System.out.println(e);
-            }
-        }
+            System.out.println("*** Failure ***");
+            
+        ArrayList<String> first_name = new ArrayList<String>();
+        ArrayList<String> last_name = new ArrayList<String>();
+        ArrayList<String> municipality = new ArrayList<String>();
+        ArrayList<String> party = new ArrayList<String>();
+        ArrayList<String> age = new ArrayList<String>();
 
         
-	}}
+        try {
+			while (result.next()) {
+			  	int Cid = result.getInt("CANDIDATE_ID");
+			      String name = result.getString("FIRST_NAME");
+			      String surname = result.getString("LAST_NAME");
+			      String region = result.getString("MUNICIPALITY");
+			      String parti = result.getString("PARTY");
+			      String vecums = result.getString("AGE");
+			      first_name.add(name);
+			      last_name.add(surname);
+			      municipality.add(region);
+			      party.add(parti);
+			      age.add(vecums);
+			      System.out.println(Cid);
+			      System.out.println(name);
+			      System.out.println(surname);
+			      System.out.println(region);
+			      
+			      
+				}
+        } catch (Exception ex1) {
+            System.out.println("*** Failure ***");
+        }
+        
+        	response.setContentType("text/plain");
+        	response.setCharacterEncoding("UTF-8");
+        	response.getWriter().print("Election Machine\r\n");
+        	
+
+        	response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            out.println("<html>");
+            out.println("<body>");
+            out.println("<ul>");
+            
+            for (int i = 0; i<first_name.size(); i++) {
+            	out.println("<li>" + first_name.get(i) + " " + last_name.get(i) + ", Age : " + age.get(i) + ", Party : " + party.get(i) +  ", Municipality: " + municipality.get(i) + "</li>");
+                out.println("<br>");
+                }
+            out.println("</ul>");
+            out.println("</body></html>");
+    
+        }
+  	}
+
+  
+
+
+        
+	}
